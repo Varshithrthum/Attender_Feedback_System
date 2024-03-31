d77bb35f4a0a325115977f07150c208bee2353a2
Caro322 <66190568+Caro322@users.noreply.github.com>
2024-03-31 14:34:15 -0500

    Update Navbarafter.js

--- src/Navbarafter.js
+++ src/Navbarafter.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from 'react-icons/fa';
+import "./Navbarbefore.css";
 
 const Navbarafter = () => {
   return (
     <div>
       <nav className="navbar">
         <div className="navdiv">
+        <div class= "logo-nav">
           <a className="navbar-brand">
             <img
               src="https://cdn.glitch.global/07931069-62a9-4bd9-a047-47fd7905975d/Oxford-Brookes-blue.jpg?v=1709197319114"
@@ -14,6 +18,22 @@ const Navbarafter = () => {
             />
             <span className="logo">Attender Feedback System</span>
           </a>
+          </div>
+          <input type="checkbox" id="check"/>
+           <div className="side-bar" id="side-bar">
+           <ul className="nav_1">
+            <li className="nav-item">
+              <a className="nav-link">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Log Out</a>
+            </li>
+          </ul>
+          </div>
+          <label for="check" class="barras">
+              <FaBars />
+          </label>
+
           <ul className="nav">
             <li className="nav-item">
               <a className="nav-link">Home</a>
